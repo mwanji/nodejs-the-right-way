74581ac8e7c70790cfdb94f6ae9fd380dd620906
moandji.ezana <mwanji@gmail.com>
2013-12-13 15:53:17 +0200

    Read file as stream

--- file-system/read.js
+++ file-system/read.js
@@ -1,11 +1,13 @@
 "use strict"
 
-const fs = require('fs');
+const fs = require('fs'),
+       stream = fs.createReadStream(process.argv[2]);
 
-fs.readFile('target.txt', function (err, data) {
-  if (err) {
-    throw err;
-  }
-  
-  console.log(data.toString());
+stream.on('data', function (chunk) {
+  process.stdout.write(chunk);
 });
+
+stream.on('error', function (err) {
+  process.stderr.write("ERROR: " + err.message + "\n");
+});
+
