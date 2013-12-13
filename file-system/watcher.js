fe52da68c09365a8dfcfac0b5f27fd7f40dcd2cf
moandji.ezana <mwanji@gmail.com>
2013-12-13 16:13:48 +0200

    Process to spawn passed as arguments

--- file-system/watcher.js
+++ file-system/watcher.js
@@ -2,15 +2,18 @@
 
 const fs = require('fs'),
        spawn = require('child_process').spawn,
-       filename = process.argv[2];
+       filename = process.argv[2],
+       command = process.argv[3],
+       commandArgs = process.argv.slice(4);
        
 if (!filename) {
   throw Error('No file given!');
 }
 
 // fs#watch only works once when the file is edited in a text editor. Cf. https://github.com/joyent/node/issues/3640#issuecomment-6806347
-fs.watchFile(filename, function () {
-  let ls = spawn('ls', ['-lh', filename]);
+fs.watch(filename, function () {
+  commandArgs.push(filename);
+  let ls = spawn(command, commandArgs);
   let output = '';
   ls.stdout.on('data', function (chunk) {
     output += chunk.toString();
