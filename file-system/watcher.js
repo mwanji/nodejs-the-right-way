6f790cc18980730c13ca904190668ac745ad1357
moandji.ezana <mwanji@gmail.com>
2013-12-13 14:59:59 +0200

    Listening to child stdout events

--- file-system/watcher.js
+++ file-system/watcher.js
@@ -8,9 +8,17 @@ if (!filename) {
   throw Error('No file given!');
 }
 
+// fs#watch only works once when the file is edited in a text editor. Cf. https://github.com/joyent/node/issues/3640#issuecomment-6806347
 fs.watchFile(filename, function () {
   let ls = spawn('ls', ['-lh', filename]);
-  ls.stdout.pipe(process.stdout);
+  let output = '';
+  ls.stdout.on('data', function (chunk) {
+    output += chunk.toString();
+  });
+  ls.stdout.on('close', function () {
+    let parts = output.split(/\s+/);
+    console.dir([parts[0], parts[4], parts[8]]);
+  });
 });
 
 console.log("Now watching " + filename + " for changes...");
