b4b2337de92239e2d430ced7ac09742da25b0188
moandji.ezana <mwanji@gmail.com>
2013-12-16 12:16:00 +0200

    Only one file watcher, regardless of number of connections

--- networking/net-watcher.js
+++ networking/net-watcher.js
@@ -2,31 +2,38 @@
 
 const net = require('net'),
        fs = require('fs'),
-       filename = process.argv[2];
+       filename = process.argv[2],
+       connections = [];
        
 if (!filename) {
   throw Error('No filename specified!');
 }
 
-const server = net.createServer(function (connection) {
-  console.log('Subscriber connected');
-  connection.write(JSON.stringify({
-    type: 'watching',
-    file: filename
-  }) + '\n');
-  
-  const watcher = fs.watch(filename, function () {
-    connection.write(JSON.stringify({
+const watcher = fs.watch(filename, function () {
+  for (let i = 0; i < connections.length; i++) {
+    connections[i].write(JSON.stringify({
       type: 'changed',
       file: filename,
       timestamp: Date.now()
     }) + '\n');
-  });
+  }
+});
+
+const server = net.createServer(function (connection) {
+  console.log('Subscriber connected');
+  connections.push(connection);
+  console.log('Connections=' + connections);
   
   connection.on('close', function () {
     console.log('Subscriber disconnected');
-    watcher.close();
+    connections.splice(connections.indexOf(connection), 1);
+    console.log('Connections=' + connections);
   });
+});
+
+server.on('close', function () {
+  watcher.close();
+  console.log('close event handler called');
 });
 
 server.listen(8000, function () {
